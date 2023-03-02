bd89014d7b94edde1fd1969a81cd3101ec812f64
tseyu <tseyu@TSEYU>
2023-03-02 19:19:35 +0800

    add calUni

--- calRa/calUni.cpp
+++ calRa/calUni.cpp
@@ -1,6 +1,12 @@
 #include "read.h"
+#include <vector>
 
-void calUni()
+
+void calUni(std::vector<double> vec)
 {
-    
+    double sum{ 0 };
+    for (int i{0}; i < vec.size();i++)
+    {
+        sum += vec[i];
+    }
 }
--- calRa/read.h
+++ calRa/read.h
@@ -1,8 +1,9 @@
 #ifndef _READ_H_
 #define _READ_H_
+#include <vector>
 
 void intro();
-void calUni();
+void calUni(std::vector<double> vec);
 
 #endif
 
